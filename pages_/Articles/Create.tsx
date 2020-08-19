9372f12ad1371c60c2979193f81cdf3b4396f543
Fluke <jesadakorn.kirtnu@gmail.com>
2020-08-19 17:05:31 +0700

    test deploy

--- pages_/Articles/Create.tsx
+++ pages_/Articles/Create.tsx
@@ -1,81 +1,37 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, createElement, } from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import styles from '../styles/styles.module.css';
 import { useCookies } from 'react-cookie';
 import { Rnd } from 'react-rnd';
-import styled from 'styled-components';
 
 const Index = () => {
     const { t } = useTranslation()
     const ref = useRef()
     const title: string = t(`common:title`)
     const [cookie, setCookie] = useCookies(["position"])
-    const [drag, setDrag] = useState({
+    const [drag, setDrag] = useState<{
+        x?: string | number;
+        y?: string | number;
+        width?: number | string;
+        height?: number | string;
+    }>({
         x: 0,
         y: 0,
         width: 0,
         height: 0,
     })
 
-    const Box = () => (
-        <div
-            className={styles.box}
-            style={{ margin: 0, height: '100%', paddingBottom: '40px' }}
-        >
-            <article className="media">
-                <div className="media-left">
-                    <figure className="image is-64x64">
-                        <img src="https://avatars1.githubusercontent.com/u/10220449?v=3&s=460" draggable="false" alt="github avatar" />
-                    </figure>
-                </div>
-                <div className="media-content">
-                    <div className="content">
-                        <p>
-                            <strong>bokuweb</strong> <small>@bokuweb17</small> <small>31m</small>
-                            <br />
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean efficitur sit amet massa fringilla egestas.
-                  Nullam condimentum luctus turpis.
-                </p>
-                    </div>
-                    <nav className="level is-mobile">
-                        <div className="level-left">
-                            <a className="level-item">
-                                <span className="icon is-small"><i className="fa fa-reply" /></span>
-                            </a>
-                            <a className="level-item">
-                                <span className="icon is-small"><i className="fa fa-retweet" /></span>
-                            </a>
-                            <a className="level-item">
-                                <span className="icon is-small"><i className="fa fa-heart" /></span>
-                            </a>
-                        </div>
-                    </nav>
-                </div>
-            </article>
-        </div>
-    );
 
-    useEffect(() => {
-        if (cookie.position) setDrag(cookie.position)
-    }, [])
-
-    return (
-        <div className={styles.container}>
-            <button onClick={() => console.log(cookie)}>Ref</button>
-            <div className={styles.navBar}><img width={200} height={200} src="/icon/Hamburger_icon.svg.png" alt="Hambuger Menu" /></div>
-            <input className={styles.header} />
-            <div contentEditable="true" ref={ref} onInput={(e) => console.log(e.currentTarget.innerText)} className={styles.content} ></div>
-            <div className={styles.footer}></div>
-
+    const dynamicCreate = () => {
+        return (
             <Rnd
                 minWidth={100}
                 minHeight={90}
                 size={{ width: drag.width, height: drag.height }}
-                position={{ x: drag.x, y: drag.y }}
+                position={{ x: +drag.x, y: +drag.y }}
                 onDragStop={(e, d) => {
-                    setDrag({ x: d.x, y: d.y })
-                    setCookie('position', { ...drag })
+                    setDrag({ ...drag, x: d.x, y: d.y })
+                    setCookie('position', JSON.stringify({ ...drag, x: d.x, y: d.y }))
                 }}
                 onResizeStop={(e, direction, ref, delta, position) => {
                     setDrag({
@@ -84,14 +40,52 @@ const Index = () => {
                         ...position
                     });
                     setCookie('position', JSON.stringify({
-                        ...drag
+                        width: ref.style.width,
+                        height: ref.style.height,
+                        ...position
                     }))
                 }}
             >
                 <div className={styles.box}>TEst</div>
             </Rnd>
+        )
+    }
 
-            <div onClick={() => console.log(JSON.parse(cookie.name))}>Test</div>
+    const Paper = () => {
+        return (
+            <div className={styles.content} >
+                <div
+                    contentEditable="true"
+                    onInput={(e) => console.log(e.currentTarget.innerText)}
+                >
+                </div>
+                <div className={styles.tools}>
+                    <button className={styles.tool}>เว้นวรรค</button>
+                    <button className={styles.tool}>เลื่อนซ้าย</button>
+                    <button className={styles.tool}>เลื่อนขวา</button>
+                    <button className={styles.tool}>ขึ้นบน</button>
+                    <button className={styles.tool}>ลงล่าง</button>
+                    <button className={styles.tool}>ขึ้นบรรทัดใหม่</button>
+                </div>
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        if (cookie.position) setDrag(cookie.position)
+    }, [])
+
+    return (
+        <div className={styles.container}>
+            <div ref={ref} className={styles.navBar}>
+                <img height="100%" src="/icon/Hamburger_icon.svg.png" alt="Hambuger Menu" />
+            </div>
+            <h1 className={styles.headerLabel}>หัวข้อ</h1>
+            <input className={styles.header} />
+            <h2 className={styles.contentLabel}>เนื้อหา</h2>
+            <Paper />
+            <div className={styles.footer}></div>
+            {false && <button onClick={() => console.log(cookie)}>Test</button>}
         </div>
     )
 }
